ecb15517db8f7082ea882a89645c24a52623d815
iqoption76 <evgen44ivan33@gmail.com>
2021-01-23 14:31:33 +0300

    Hello World

--- get-quote.py
+++ get-quote.py
@@ -1,4 +1,4 @@
-def index():
+def primary():
    print("Keep it logically awesome.")
 
   #f = open("quotes.txt")
@@ -8,4 +8,4 @@ def index():
   #print(quotes)
 
 if __name__== "__main__":
-  index()
+  primary()
